57c0e8f0f590f9154150c3e8a3f9c7fbd3bed5c0
Michał Górny <mgorny@gentoo.org>
2021-04-02 22:35:43 +0200

    Include schema version in database
    Signed-off-by: Michał Górny <mgorny@gentoo.org>

--- kuroneko/database.py
+++ kuroneko/database.py
@@ -6,6 +6,8 @@
 import json
 import typing
 
+from kuroneko import __version__
+
 
 class Bug(typing.NamedTuple):
     """Tuple representing bug in the database."""
@@ -17,10 +19,16 @@ class Bug(typing.NamedTuple):
     created: str
 
 
+class DatabaseError(RuntimeError):
+    """Error loading the database."""
+
+
 class Database:
     """Bug database."""
 
     bugs: typing.Dict[int, Bug]
+
+    SCHEMA_VERSION = [int(x) for x in __version__.split('.', 2)[:2]]
 
     def __init__(self) -> None:
         """Init an empty database."""
@@ -28,14 +36,30 @@ class Database:
 
     def load(self, fileobj: typing.IO) -> None:
         """Load database from open JSON file."""
-        self.bugs = {}
         data = json.load(fileobj)
+        try:
+            kv = data['kuroneko-version']
+            if not isinstance(kv, str):
+                raise ValueError()
+            major, minor = (int(x) for x in kv.split('.', 1))
+        except KeyError:
+            raise DatabaseError('No kuroneko-version in database file')
+        except ValueError:
+            raise DatabaseError('Incorrect kuroneko-version value')
+        if major != self.SCHEMA_VERSION[0]:
+            raise DatabaseError(f'Incompatible database schema {major}.'
+                                f'{minor} ({self.SCHEMA_VERSION} is '
+                                f'the current version')
+
+        self.bugs = {}
         for bug in data['bugs']:
             self.bugs[bug['bug']] = Bug(**bug)
 
     def save(self, fileobj: typing.IO) -> None:
         """Save database into open JSON file."""
         json.dump({
+            'kuroneko-version': '.'.join(str(x)
+                                         for x in self.SCHEMA_VERSION),
             'bugs': list(x._asdict() for x in self.bugs.values()),
             }, fileobj)
 
